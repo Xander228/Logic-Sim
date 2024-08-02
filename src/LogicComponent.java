c90852ab041527ffab50ad01c30bcb0774d0cdfb
Thadeus Bates <batesthadeus@gmail.com>
2024-08-02 15:29:38 -0700

    update

--- src/LogicComponent.java
+++ src/LogicComponent.java
@@ -2,66 +2,78 @@ import javax.swing.*;
 import javax.swing.border.MatteBorder;
 import java.awt.*;
 import java.awt.event.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 public class LogicComponent extends JComponent {
     Color color;
-    final boolean draggable;
+    boolean draggable;
     int mouseX, mouseY;
-    int x,y;
-    boolean isNew;
+    int startingX, startingY;
+
 
     LogicComponent(Boolean draggable, Color color) {
-        isNew = true;
         setPreferredSize(new Dimension(100,40));
         setMaximumSize(new Dimension(100,40));
         setBorder(new MatteBorder(1,1,1,1,Color.ORANGE));
         this.color = color;
         this.draggable = draggable;
+        Point p = MouseInfo.getPointerInfo().getLocation();
+        mouseX = p.x;
+        mouseY = p.y;
 
         EventQueue.invokeLater(new Runnable() {
             public void run() {
                 addListeners();
             }
         });
-
     }
 
     private void addListeners(){
         JLayeredPane pane = (JLayeredPane) SwingUtilities.getAncestorOfClass(JLayeredPane.class, this);
-        pane.addMouseListener(new MouseAdapter(){
+         pane.addMouseListener(new MouseAdapter() {
             @Override
             public void mousePressed(MouseEvent e) {
-                int eventX = e.getXOnScreen();
-                int eventY = e.getYOnScreen();
-                if(!isHovered(eventX, eventY)) return;
-
-                mouseX = eventX;
-                mouseY = eventY;
-
-                x = getX();
-                y = getY();
-
                 if(!draggable) createChild();
             }
-        });
-
-        if (draggable) pane.addMouseMotionListener(new MouseMotionAdapter() {
+             @Override
+             public void mouseReleased(MouseEvent e) {
+                 if(draggable) destruct();
+             }
+         });
+        pane.addMouseMotionListener(new MouseMotionAdapter() {
             @Override
             public void mouseDragged(MouseEvent e) {
+                if(!draggable) return;
                 int eventX = e.getXOnScreen();
                 int eventY = e.getYOnScreen();
                 if(!isHovered(eventX, eventY)) return;
-
                 int deltaX = eventX - mouseX;
                 int deltaY = eventY - mouseY;
-                setLocation(x + deltaX, y + deltaY);
+                setLocation(startingX + deltaX, startingY + deltaY);
             }
+
         });
     }
+
+
+    public void setStartingLocation(Point p){
+        startingX = p.x;
+        startingY = p.y;
+        setLocation(startingX,startingY);
+    }
+
+    public void destruct(){
+        JLayeredPane pane = (JLayeredPane) SwingUtilities.getAncestorOfClass(JLayeredPane.class, this);
+        if(pane != null) {
+            pane.remove(this);
+            pane.revalidate();
+            pane.repaint();
+        }
+
+    }
+
+
 
     public boolean isHovered(Point p){
         SwingUtilities.convertPointFromScreen(p, this);
@@ -76,16 +88,17 @@ public class LogicComponent extends JComponent {
 
     public void createChild(){
         JLayeredPane pane = (JLayeredPane) SwingUtilities.getAncestorOfClass(JLayeredPane.class, this);
+        Point p = getLocationOnScreen();
+        SwingUtilities.convertPointFromScreen(p, pane);
+
         LogicComponent logicComponent = new LogicComponent(true,Color.BLUE);
         pane.add(logicComponent);
         pane.setLayer(logicComponent, JLayeredPane.DRAG_LAYER, 0);
-        Point p = getLocationOnScreen();
-        SwingUtilities.convertPointFromScreen(p, pane);
-        logicComponent.revalidate();
 
+        logicComponent.revalidate();
         EventQueue.invokeLater(new Runnable() {
             public void run() {
-                logicComponent.setLocation(p);
+                logicComponent.setStartingLocation(p);
             }
         });
 
